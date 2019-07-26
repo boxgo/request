d73118d1aa0776fa1a003fbf9a088c4102018974
bitebit <kfbygj@gmail.com>
2019-07-26 15:23:21 +0800

    feat(logger): Support trace

--- logger.go
+++ logger.go
@@ -12,13 +12,13 @@ func logBefore(agent *gorequest.SuperAgent) {
 
 	if GlobalOptions.ShowLog {
 		curl, _ := agent.AsCurlCommand()
-		logger.Default.Infow("request_start", "curl", curl)
+		logger.Trace(agent.Context).Infow("request_start", "curl", curl)
 	}
 }
 
 func logAfter(agent *gorequest.SuperAgent, resp gorequest.Response, body []byte, errs []error) {
 	if GlobalOptions.ShowLog {
 		curl, _ := agent.AsCurlCommand()
-		logger.Default.Infow("request_end", "curl", curl, "errs", errs, "resp.status", resp.StatusCode, "body", string(body[:]), "resp.header", resp.Header)
+		logger.Trace(agent.Context).Infow("request_end", "curl", curl, "errs", errs, "resp.status", resp.StatusCode, "body", string(body[:]), "resp.header", resp.Header)
 	}
 }
